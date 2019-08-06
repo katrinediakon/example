bb91616ebe8b1a5a9ff6404c8c42ee46a030f9d5
Kate <Katrinekatek@gmail.com>
2019-08-06 13:48:47 +0300

    t/f

--- function.php
+++ function.php
@@ -1,7 +1,9 @@
 <?php
-function declOfNum($number, $one, $two, $tree)
+function declOfNum($number, $one, $two, $tree, $showNumber)
 {
   $str="";
+  if($showNumber==true)
+  {
   if($number==0||$number==5)
   {
     $str=$number ." ". $tree;
@@ -14,15 +16,31 @@ function declOfNum($number, $one, $two, $tree)
   {
     $str=$number." ". $two;
   }
+}
+else
+{
+if($number==0||$number==5)
+{
+  $str= $tree;
+}
+if($number==1)
+{
+  $str= $one;
+}
+if($number==22)
+{
+  $str= $two;
+}
+}
   echo $str;
   echo "</br>";
 }
 
 //примеры вывода:
-declOfNum(0, 'элемент', 'элемента', 'элементов');  //0 элементов
-declOfNum(1, 'элемент', 'элемента', 'элементов');  //1 элемент
-declOfNum(5, 'элемент', 'элемента', 'элементов');  //5 элементов
-declOfNum(22, 'элемент', 'элемента', 'элементов'); //22 элемента
+declOfNum(0, 'элемент', 'элемента', 'элементов', true);  //0 элементов
+declOfNum(1, 'элемент', 'элемента', 'элементов', false); //элемент
+declOfNum(5, 'элемент', 'элемента', 'элементов', false); //элементов
+declOfNum(22, 'элемент', 'элемента', 'элементов', true); //22 элемента
 
 
  ?>
